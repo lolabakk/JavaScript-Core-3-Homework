d0b0adc77c2880243f304b3d13da7de680983b04
Lola Bello <mololabello@gmail.com>
2020-09-14 00:44:40 +0100

    show list update

--- week-2/Homework/mandatory/3-dog-photo-gallery/dog-script.js
+++ week-2/Homework/mandatory/3-dog-photo-gallery/dog-script.js
@@ -1,20 +1,23 @@
-fetch(`https://dog.ceo/api/breeds/image/random`)
-    .then(function (response) {
-        return response.data();
+let clickButton = document.querySelector(".clickButton");
+clickButton.addEventListener("click", getNewDogImage)
+let dogSelector = document.querySelector(".dogSelector");
+let dogItems = document.createElement("ul");
+let dogLists = document.createElement("li");
+let dogImage = document.createElement("img");
+
+function getNewDogImage() {
+
+
+  fetch(`https://dog.ceo/api/breeds/image/random`)
+    .then(response => response.json())
+    .then(data => {
+      dogImage.src = data.message;
+      dogSelector.appendChild(dogItems);
+      dogItems.appendChild(dogLists);
+      dogLists.appendChild(dogImage);
+
     })
-    .then(function (data) {
-        console.log(data);
-   
-             let getDogImages = document.createElement("img");
-             getDogImages.src = episode.image.medium;
-             let dogShow = document.createElement("ul");
-             let dogLists = document.createElement("li");
-      
-
- })
-
-        // Write the code to display the greeting text here
-  
     .catch(function (error) {
-        console.log(error);
-    });
+      console.log(error)
+    })
+}
